7c43ff49ca23e797ba8d2bfaef26380e3da6028f
ZJay <wangpc.hex@gmail.com>
2023-02-02 15:07:54 +0800

    增加新的版本的 Scrumdinger 项目, 主要修改了原有工程缺少颜色定义和图片文件的问题, 文档尚存在未完成的部分

---
+++ practice/Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,39 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by ZJay on 2023/2/1.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+                
+            }
+        }
+        .navigationTitle("Daily Scrum")
+        .toolbar {
+            Button(action: {}) {
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
+    }
+}
